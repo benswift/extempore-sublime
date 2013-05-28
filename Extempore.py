af5717a360e97f462db7da92d988ebfe1e4fb78d
Ben Swift <benjamin.j.swift@gmail.com>
2013-05-28 16:32:53 +1000

    it works (and I *think* multiple connections work, too)

--- Extempore.py
+++ Extempore.py
@@ -1,78 +1,89 @@
 import sublime, sublime_plugin
 import socket
+import re
 
-# a dict (of sockets) for keeping track of all the connections
-EXTEMPORE_CONNECTIONS = {}
+SETTINGS_FILE = 'Extempore.sublime-settings'
+EXTEMPORE_SOCKETS = {}
 
-class ExtemporeConnectCommand(sublime_plugin.WindowCommand):
+# currently, the host and port are hardcoded
+# h, p = host_str.split(':')
+# self.window.show_input_panel("Specify host:port", "localhost:7099", self.on_done, None, None)
+
+def get_extempore_socket(current_view):
+	return EXTEMPORE_SOCKETS[current_view.id()]
+
+def is_currently_connected(current_view):
+	try:
+		get_extempore_socket(current_view)
+		return True
+	except KeyError:
+		return False
+
+def set_extempore_socket(current_view, sock):
+	if current_view.id() in EXTEMPORE_SOCKETS:
+		sublime.status_message("This view is already connected to Extempore")
+	else:
+		EXTEMPORE_SOCKETS[current_view.id()] = sock
+
+class ExtemporeConnectCommand(sublime_plugin.TextCommand):
 	"""Connect to a running Extempore server"""
-	host = None
-
-	def run(self):
-		if self.window.active_view().id() not in EXTEMPORE_CONNECTIONS:
-			# self.window.show_input_panel("Specify host:port", "localhost:7099", self.get_host, None, None)
+	def run(self, edit):
+		if not is_currently_connected(self.view):		
 			self.connect(('localhost', 7099))
-
-	def get_host(self, hoststring):
-		h, p = hoststring.split(':')
-		self.host = (h, int(p))
 
 	def connect(self, host):
 		try:
-			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			sock.settimeout(5)
-			sock.connect(host)
-			data = sock.recv(1024)
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(5)
+			s.connect(host)
+			data = s.recv(1024)
 			sublime.status_message(data)
-			# add the socket to the global connections dict
-			EXTEMPORE_CONNECTIONS[self.window.active_view().id()] = sock
-		except socket.error, e:
+			# add this socket to the global socket dict
+			set_extempore_socket(self.view, s)
+		except socket.error as e:
 			sublime.status_message("Unable to connect to Extempore server: " + repr(e))
 			return None
 
-class ExtemporeDisconnectCommand(sublime_plugin.WindowCommand):
-	"""Disconnect from the Extempore server"""
-	def run(self, edit):
-		view_id = self.window.active_view().id()
-		if view_id in EXTEMPORE_CONNECTIONS:
+	def disconnect(self):
 			try:
-				EXTEMPORE_CONNECTIONS[view_id].close()
+				s = get_extempore_socket(self.view)
+				s.close()
 				sublime.status_message("Closed connection to Extempore server")
-			except socket.error, e:
-				print repr(e)
+			except socket.error as e:
+				sublime.status_message("Unable to disconnect: " + repr(e))
 
 class ExtemporeEvaluateCommand(sublime_plugin.TextCommand):
 	"""Send the current defn/region to the Extempore server for evaluation"""
-	# def __init__(self, view):
-	# 	sublime_plugin.TextCommand.__init__(self, view)
+	def __init__(self, view):
+		sublime_plugin.TextCommand.__init__(self, view)
 
-	def defun_bounds(self):
-		# todo implement this!
-		pass
+	def send_string_for_eval(self, string):
+		try:
+			s = get_extempore_socket(self.view)
+			s.send(string + '\r\n')
+			return s.recv(1024)
+		except KeyError:
+			sublime.status_message("Error: cannot find an Extempore connection for this view")
+		except socket.error as e:
+			sublime.status_message("Error in connection to Extempore server: " + repr(e))
 
-	def send(self, sock, string):
-		sock.send(string + '\r\n')
-		return sock.recv(1024)
-		
+	def toplevel_defun_region(self):
+		v = self.view
+		while not re.match(r'^\(', v.substr(v.sel()[0])):
+			v.run_command("expand_selection", {"to": "brackets"})
+
 	def run(self, edit):
-		sels = self.view.sel()
-		sock = ExtemporeServerConnection.s
+		v = self.view
+		v.run_command("single_selection")
 
-		if not sock:
-			sublime.status_message("Not currently connected, trying to connect...")
-			self.view.run_command('extempore_connect')
-			sock = ExtemporeServerConnection.s
+		if len(v.sel()[0]) == 0:
+			# if no region highlighted, select the current 'top level' defun, otherwise just use the current selection
+			self.toplevel_defun_region()
 
-		if sock:
-			for sel in sels:
-				string = self.view.substr(sel)
-				if len(string) > 0:
-					try:
-						response = self.send(sock,string)
-						sublime.status_message(response)
-						self.view.sel().clear()
-					except Exception, e:
-						sublime.status_message("No response from socket")
-		else:
-			sublime.error_message("Error: no available for Extempore connection")
-
+		# send the region to the Extempore server for evaluation
+		try:
+			response = self.send_string_for_eval(v.substr(v.sel()[0]))
+			sublime.status_message(response)
+			v.sel()[0].clear()
+		except socket.error as e:
+			sublime.status_message("No response from socket: {1}".format(e))
